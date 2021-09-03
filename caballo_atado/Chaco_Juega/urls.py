da1cf05767267f28bb823fc8aaedd6e6f9e754af
alfeinfo <alfedoinfo1485@gmail.com>
2021-09-03 11:17:11 -0300

    modifique las clases

--- caballo_atado/Chaco_Juega/urls.py
+++ caballo_atado/Chaco_Juega/urls.py
@@ -1,9 +1,19 @@
-
-    
 from django.urls import path
 
 from . import views
-app_name= 'Chaco_Juega'
+
+app_name = 'Chaco_Juega'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+
+
+
+"""app_name= 'Chaco_Juega'
 
 urlpatterns = [
     # le da vida: /juego/
--- caballo_atado/Chaco_Juega/views.py
+++ caballo_atado/Chaco_Juega/views.py
@@ -1,34 +1,61 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-from .models import Question
-from django.http import Http404
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
 
 
+class IndexView(generic.ListView):
+    template_name = 'Chaco_Juega/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template= loader.get_template('Chaco_Juega/index.html')
-    context={
-        'latest_question_list' : latest_question_list,
-    }
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(template.render(context,request))
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'Chaco_Juega/index.html', context)
 
-def detail(request, question_id):
-    return HttpResponse("Estas viendo las preguntas %s." % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'Chaco_Juega/detail.html'
 
-def results(request, question_id):
-    response = "Estas viendo los resultados de las preguntas %s."
-    return HttpResponse(response % question_id)
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'Chaco_Juega/results.html'
+
 
 def vote(request, question_id):
-    return HttpResponse("Estas votando sobre la pregunta %s." % question_id)
+    ... # same as above, no changes needed.
+
+
+
+
+
+
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'Chaco_Juega/detail.html', {
+            'question': question,
+            'error_message': "No seleccionaste una opción.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('Chaco_Juega:results', args=(question.id,)))
+
+
+
+
 
 def detail(request, question_id):
     try:
